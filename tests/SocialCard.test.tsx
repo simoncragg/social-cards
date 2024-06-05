9b897918dfd6b1fdc96efc45d7f0a948b8c6ccf8
BradWhite93 <BradWhite93@users.noreply.github.com>
2024-06-05 17:42:16 +0100

    test: add another test for optional props

--- tests/SocialCard.test.tsx
+++ tests/SocialCard.test.tsx
@@ -1,10 +1,18 @@
-import { expect, test } from 'vitest';
+import { expect, describe, test } from 'vitest';
 import SocialCard from '../src/components/SocialCard';
 import { render, screen } from '@testing-library/react';
 
-test('Given social card is rendered', () => {
-  render(<SocialCard username='Bob' title='test title' message='test message' />);
-  expect(screen.getByText('Bob')).toBeInTheDocument();
-  expect(screen.getByText('test title')).toBeInTheDocument();
-  expect(screen.getByText('test message')).toBeInTheDocument();
+describe('Social card', () => {
+  test('should render all expected props', () => {
+    render(<SocialCard username='Bob' title='test title' message='test message' />);
+    expect(screen.getByText('Bob')).toBeInTheDocument();
+    expect(screen.getByText('test title')).toBeInTheDocument();
+    expect(screen.getByText('test message')).toBeInTheDocument();
+  });
+  test('should not render title when not provided', () => {
+    render(<SocialCard username='Bob' message='test message' />);
+    expect(screen.getByText('Bob')).toBeInTheDocument();
+    expect(screen.queryByText('test title')).not.toBeInTheDocument();
+    expect(screen.getByText('test message')).toBeInTheDocument();
+  });
 });
