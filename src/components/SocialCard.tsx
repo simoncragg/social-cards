e118502fe9ad0baf31941a4a2cf1b1e31386de3f
Bradley White <Bradley.White@ao.com>
2024-06-06 17:39:06 +0100

    style: social card elements

--- src/components/SocialCard.tsx
+++ src/components/SocialCard.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React from "react";
 
 interface SocialCardProps {
-    username: string;
-    title?: string;
-    message: string;
-    userId: number;
+  username: string;
+  title?: string;
+  message: string;
+  userId: number;
 }
+//Add a div or overarching container? to hold image/name?
 
-const SocialCard: React.FC<SocialCardProps> = ({ username, title, message, userId }) => {
+const SocialCard: React.FC<SocialCardProps> = ({
+  username,
+  title,
+  message,
+  userId,
+}) => {
   return (
-      <div className="border border-white p-4 rounded-xl mb-8">
-        <img className="rounded-full w-12 h-12" data-testid={`profile-pic-${userId}`} src={`https://xsgames.co/randomusers/assets/avatars/pixel/${userId}.jpg`}/>
-        <h1 className="text-3xl font-bold">{ username }</h1>
-        {title && <p>{ title }</p>}
-        <p>{ message }</p>
+    <div className="flex flex-col border border-white p-4 rounded-xl mb-8 items-start">
+      <div className="flex items-center mb-2">
+        <img
+          className="rounded-full w-6 h-6"
+          data-testid={`profile-pic-${userId}`}
+          src={`https://xsgames.co/randomusers/assets/avatars/pixel/${userId}.jpg`}
+        />
+        <span className="text-xl font-bold ml-4">{username}</span>
       </div>
+      {title && <p className="font-semibold">{title}</p>}
+      <p>{message}</p>
+    </div>
   );
 };
 
