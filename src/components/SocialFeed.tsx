7d433ed7cab92306eadf971acff8cb547296df7f
Bradley White <Bradley.White@ao.com>
2024-06-07 17:09:22 +0100

    feat: add routing for new post (echo)

--- src/components/SocialFeed.tsx
+++ src/components/SocialFeed.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import SocialCard from "./SocialCard";
 import { getCards } from "../repositories/SocialCardRepository";
+import { FiPlusCircle } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 
 const SocialFeed: React.FC = () => {
+  const navigate = useNavigate();
   const cards = getCards();
-
-  return cards.map((card) => (
-    <SocialCard
-      username={card.username}
-      title={card.title}
-      message={card.message}
-      userId={card.userId}
-    />
-  ));
+  return (
+    <>
+      <button type="button" onClick={() => navigate("/new-echo")}>
+        <FiPlusCircle className="w-8 h-8 text-cyan-500" />
+      </button>{" "}
+      {cards.map((card) => (
+        <SocialCard
+          username={card.username}
+          title={card.title}
+          message={card.message}
+          userId={card.userId}
+        />
+      ))}
+    </>
+  );
 };
 
 export default SocialFeed;
