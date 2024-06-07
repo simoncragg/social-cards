84fc64a790271e628a0777db0ccf43973547d41d
BradWhite93 <BradWhite93@users.noreply.github.com>
2024-06-07 17:09:22 +0100

    feat: add routing for new post (echo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
+import NewPost from './components/NewPost';
 import SocialFeed from './components/SocialFeed';
+import {
+  Navigate,
+  Route,
+  Routes
+} from "react-router-dom";
 
 const App = () => {
-  return <SocialFeed />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/echos" replace/>}/>
+      <Route path="/echos" element={<SocialFeed />} />
+      <Route path="/new-echo" element={<NewPost />} />
+    </Routes>
+  )
 };
 
 export default App;
