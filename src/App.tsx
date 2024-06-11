33b8a99f7074c849f5d36243c945cb43adc5ea62
BradWhite93 <BradWhite93@users.noreply.github.com>
2024-06-11 16:52:51 +0100

    refactor: move router closer to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import NewPost from './components/NewPost';
 import SocialFeed from './components/SocialFeed';
 import {
+  BrowserRouter,
   Navigate,
   Route,
   Routes
@@ -9,11 +10,13 @@ import {
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigate to="/echos" replace/>}/>
       <Route path="/echos" element={<SocialFeed />} />
       <Route path="/new-echo" element={<NewPost />} />
     </Routes>
+    </BrowserRouter>
   )
 };
 
