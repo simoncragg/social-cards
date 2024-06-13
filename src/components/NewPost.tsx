f5f2472ca39796749f9c1bfa611768676d642300
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-06-13 23:41:38 +0100

    style: new post title

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,5 +1,9 @@
 const NewPost = () => {
-    return <div>New Post</div>;
+    return (
+      <div className="w-full md:w-1/2">
+        <h1 className="text-2xl font-semibold">New Echo</h1>
+      </div>
+    );
 };
 
 export default NewPost;
