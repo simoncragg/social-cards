f4d7f191dd8744ca8c4a67844453c617a78b2df4
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-06-14 11:26:32 +0100

    feat: add todo line

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,9 +1,10 @@
 const NewPost = () => {
-    return (
-      <div className="w-full md:w-1/2">
-        <h1 className="text-2xl font-semibold">New Echo</h1>
-      </div>
-    );
+  return (
+    <div className="w-full md:w-1/2">
+      <h1 className="text-2xl font-semibold mb-8">New Echo</h1>
+      <p>🚧 todo</p>
+    </div>
+  );
 };
 
 export default NewPost;
